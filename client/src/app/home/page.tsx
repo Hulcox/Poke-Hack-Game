b323251147cb1fdaf1996ff050d1a05c09e006f0
Romain BIDAULT <63655214+Hulcox@users.noreply.github.com>
2025-02-06 14:16:28 +0100

    Starting home screen with nav bar drawer and categories

---
+++ client/src/app/home/page.tsx
@@ -0,0 +1,20 @@
+const Home = () => {
+  return (
+    <div className="bg-neutral-content w-full h-full p-8 text-white grid grid-flow-col grid-rows-3 gap-8">
+      <div className="bg-neutral rounded-box transition-all hover:scale-[1.02] p-4 ring-4 ring-base-100 shadow-md shadow-black cursor-pointer">
+        ME
+      </div>
+      <div className="bg-neutral rounded-box transition-all hover:scale-[1.02] p-4">
+        FRIEND LIST
+      </div>
+      <div className="bg-neutral rounded-box transition-all hover:scale-[1.02] p-4 col-span-2">
+        MY TEAMS
+      </div>
+      <div className="bg-neutral rounded-box transition-all hover:scale-[1.02] p-4 row-span-2">
+        WEATHER
+      </div>
+    </div>
+  );
+};
+
+export default Home;
