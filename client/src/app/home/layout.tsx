1148d48cc035ef07a8c61874d2c3f0799233e721
Romain BIDAULT <63655214+Hulcox@users.noreply.github.com>
2025-02-06 14:16:28 +0100

    Starting home screen with nav bar drawer and categories

---
+++ client/src/app/home/layout.tsx
@@ -0,0 +1,29 @@
+import NavBar from "@/components/navBar";
+import { ReactNode } from "react";
+
+const HomeLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <div className="drawer lg:drawer-open">
+      <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content flex flex-col items-center justify-cente">
+        {children}
+        <label
+          htmlFor="my-drawer-2"
+          className="btn btn-primary drawer-button lg:hidden"
+        >
+          Open drawer
+        </label>
+      </div>
+      <div className="drawer-side">
+        <label
+          htmlFor="my-drawer-2"
+          aria-label="close sidebar"
+          className="drawer-overlay"
+        ></label>
+        <NavBar />
+      </div>
+    </div>
+  );
+};
+
+export default HomeLayout;
